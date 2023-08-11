322a9062482f73cbdd72c6d30526f7045b8406e8
Liquid Internet <87697556+liquidinternet@users.noreply.github.com>
2023-08-11 16:29:02 +0100

    Added vector support

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,35 @@
+#include "colour.h"
+#include "vec3.h"
 #include <iostream>
 
 int main() {
 
+	// image dimensions
+	int image_width = 256;
+	int image_height = 256;
+
+	// render image
+	std::cout << "P3\n"
+			  << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = 0; j < image_height; j++) {
+
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
+		for (int i = 0; i < image_width; i++) {
+
+			auto pixel_colour = colour(
+										double(i) / (image_width - 1),
+										double(j) / (image_height - 1),
+										0
+										);
+			write_colour(std::cout, pixel_colour);
+
+		}
+	}
+
+	std::clog << "\rDone.\n";
+
 	// all done
 	return 0;
-
 }
