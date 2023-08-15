72accc7f0638d720d85dd30748602411f42aff7e
Liquid Internet <87697556+liquidinternet@users.noreply.github.com>
2023-08-15 14:33:59 +0100

    Formatting fix

--- src/main.cc
+++ src/main.cc
@@ -9,37 +9,32 @@ int main() {
 	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
 
 	for (int a = -11; a < 11; a++) {
-		for (int b = -11; b < 11; b++)
-		{
+		for (int b = -11; b < 11; b++) {
+
 			auto choose_mat = random_double();
 			point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
 
-			if ((center - point3(4, 0.2, 0)).length() > 0.9)
-			{
+			if ((center - point3(4, 0.2, 0)).length() > 0.9) {
 				shared_ptr<material> sphere_material;
 
-				if (choose_mat < 0.8)
-				{
-					// diffuseˆ
+				if (choose_mat < 0.8) {
+					// diffuse
 					auto albedo = colour::random() * colour::random();
 					sphere_material = make_shared<lambertian>(albedo);
 					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
-				else if (choose_mat < 0.95)
-				{
+				} else if (choose_mat < 0.95) {
 					// metal
 					auto albedo = colour::random(0.5, 1);
 					auto fuzz = random_double(0, 0.5);
 					sphere_material = make_shared<metal>(albedo, fuzz);
 					world.add(make_shared<sphere>(center, 0.2, sphere_material));
-				}
-				else
-				{
+				} else {
 					// glass
 					sphere_material = make_shared<dielectric>(1.5);
 					world.add(make_shared<sphere>(center, 0.2, sphere_material));
 				}
 			}
+
 		}
 	}
 
